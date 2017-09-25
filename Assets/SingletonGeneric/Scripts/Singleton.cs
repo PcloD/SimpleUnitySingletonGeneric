464443c1639e1dc9d873b2584e659abc7a9398da
Augusto Brito <augustobrito@outlook.com>
2017-09-24 23:41:55 -0300

    Fix

--- Assets/SingletonGeneric/Scripts/Singleton.cs
+++ Assets/SingletonGeneric/Scripts/Singleton.cs
@@ -44,10 +44,11 @@ public class Singleton<T> : MonoBehaviour where T : Singleton<T>
         }
         else
         {
-            if (this == _instance) return;
-
-            Debug.Log("More then one instance of: " + Type.Name + " this instance will be deleted to preserve only one");
-            Destroy(this.gameObject);
+            if (this != _instance)
+            {
+                Debug.Log("More then one instance of: " + Type.Name + " this instance will be deleted to preserve only one");
+                Destroy(this.gameObject);
+            }
         }
     }
 }
